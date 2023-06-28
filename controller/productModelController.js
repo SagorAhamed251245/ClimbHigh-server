094c4adc8dc1dca2408d651ec2452283e9e3b103
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-06-28 20:38:08 +0600

    modify

--- controller/productModelController.js
+++ controller/productModelController.js
@@ -28,10 +28,21 @@ export const addProduct = async (req, res) => {
 
 export const getAllProducts = async (req, res) => {
   try {
-    const result = await productModel.find().toArray();
-    res.send(result);
+    const products = await productModel.find();
+    const randomProducts = shuffleArray(products);
+    res.send(randomProducts);
   } catch (error) {
     console.error(error);
-    res.status(500).send("An error occurred while adding the product.");
+    res.status(500).send("An error occurred while fetching the products.");
   }
 };
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
